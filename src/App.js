52c72143b6e533e7738ddfa858e9db4a7efe1481
Orivaldo Santana Jr <orivaldo@gmail.com>
2020-10-18 23:32:43 -0300

    funcionando na web (githubpage)
    Com protocolo wss no broker da mosquitto.org

--- src/App.js
+++ src/App.js
@@ -5,11 +5,9 @@ import Status from './Status';
 import BasicControl from './components/BasicControl';
 import RobotStatus from './components/RobotStatus';
 
-
-
 export default function App() {
   return (
-    <Connector brokerUrl="ws://157.230.89.7:1884" opts={{ username: 'mqtt', password: 'oriva_mqtt' }} >
+    <Connector brokerUrl="wss://test.mosquitto.org:8081" >
         <h1>Terminal URA MQTT</h1>
         <Status />
         <BasicControl />
